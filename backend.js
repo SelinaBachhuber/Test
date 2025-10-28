eac40962fc2d39d6ece866e20cf105efbac40ca0
SelinaBachhuber <135033598+SelinaBachhuber@users.noreply.github.com>
2025-10-28 17:19:09 +0100

    Dropdown Menü in der index.html

--- backend.js
+++ backend.js
@@ -1,12 +1,18 @@
-<script src="https://cdn.emailjs.com/dist/email.min.js"></script>
+//Dropdown Menü
+    function toggleDropdown() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    }
 
-  emailjs.init("DEIN_USER_ID"); // z.B. "uXyz123ABC"
-
-  document.getElementById("contact-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    emailjs.sendForm("service_nk1b48p", "template_xyz", this)
-      .then(() => alert("Nachricht erfolgreich gesendet!"))
-      .catch(() => alert("Fehler beim Senden. Bitte erneut versuchen."));
-  });
-
+    // Klick außerhalb schließt das Menü
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+          let openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
+//////////////
